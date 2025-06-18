e035a931360f7b764c8d8a393fd80ec7587deee9
Nyampundu <113338566+Kittelsaa@users.noreply.github.com>
2025-06-18 17:44:41 +0200

    updates

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export const SITE_TITLE = 'Personal Website Template';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
---
+++ src/pages/rss.xml.js
@@ -0,0 +1,17 @@
+import rss from "@astrojs/rss";
+import { SITE_TITLE } from "../config";
+import { getCollection } from "astro:content";
+
+export async function get(context) {
+  const blog = await getCollection("blog");
+  return rss({
+    title: SITE_TITLE,
+    site: import.meta.env.SITE,
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/blog/${post.slug}/`,
+    })),
+  });
+}
